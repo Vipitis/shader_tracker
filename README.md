7817506e28b8dd89ee7d2a2fe8d1f6fdae450672
Jan <jakel101@hhu.de>
2024-12-21 01:11:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Shader-track?
+
+WIP utility to track your public shadertoy shaders in a github repository.
+
+### some goals:
+
+* [ ] download all public shaders via the API
+* [ ] also accept the "Export All Shaders" / "Export All Public Shaders" buttons on your user profile
+* [ ] order all your shaders into folders (with image.frag, buffer_a.frag, etc.) with a json too
+* [ ] have a markdown gallery with gif previews to easily browse
+* [ ] be able to set some keywords/tags to not show in the gallery
+* [ ] launch script for wgpu-shadertoy
+* [ ] only update recent shaders (by a given time, or most recent range, dynamically backtrack untill there is no more change +/- puffer?)
+* [ ] update specific shaders (by id/link)
+* [ ] (maybe) statistics over time/change?
+
+
+### how to use:
+```bash
+python gather.py
+```
+tbd.
+
+### requirements:
+* [python requierements](/requierements.txt)
+* `SHADERTOY_KEY` env var to access the API
---
+++ gather.py
@@ -0,0 +1,57 @@
+import datetime
+import json
+import os
+import requests
+from tqdm.auto import tqdm
+
+# so we don't have to write the same code...
+from wgpu_shadertoy.api import _get_api_key, shadertoy_from_id
+
+USER_NAME = "jakel101" # maybe let the user configure this somehow?
+HEADERS = {"user-agent": "https://github.com/vipitis/shader-tracker script"}
+API_KEY = _get_api_key()
+
+# documentation: https://www.shadertoy.com/howto#q2
+# TODO: find better query that avoids false hits?
+def get_users_shader_ids(user_name):
+    url = f"https://www.shadertoy.com/api/v1/shaders/query/{user_name}"
+    response = requests.get(url, params={"key": API_KEY, "sort": "newest"}, headers=HEADERS)
+    if response.status_code != 200:
+        raise Exception(f"Failed to get shaders for user {user_name}: {response.text}")
+    data = json.loads(response.text)
+    return [shader for shader in data["Results"]]
+
+
+if __name__ == "__main__":
+    shader_ids = get_users_shader_ids(USER_NAME)
+    all_data = []
+    for shader_id in tqdm(shader_ids):
+        shader_data = shadertoy_from_id(shader_id)
+        all_data.append(shader_data)
+
+    # closely mirror the of the export button, API differs in several poins:
+    # fields like "published", input/output "id" use different values (simple mapping)
+    # "src" and "ctype" use different names
+    # plenty of fields missing in the download...
+    construct = {
+        "userName": USER_NAME,
+        "date": datetime.datetime.now(datetime.timezone.utc).isoformat(),
+        "numShaders": len(all_data),
+        "shaders": [s["Shader"] for s in all_data]
+    }
+    with open(f"{USER_NAME}_shaders.json", "w") as f:
+        json.dump(construct, f, indent=2)
+
+    # make one folder per shader?
+    os.makedirs(USER_NAME, exist_ok=True)
+    for shader in all_data:
+        shader_path = os.path.join(USER_NAME, shader["Shader"]["info"]["id"])
+        with open(shader_path, "w") as f:
+            json.dump(shader, f, indent=2)
+        for renderpass in shader["Shader"]["renderpass"]:
+            code, name = renderpass["code"], renderpass["name"]
+            with open(os.path.join(shader_path, f"{name.replace(" ", "_")}.frag"), "w") as f:
+                f.write(code)
+    
+    # TODO: add gallyer.md with a nice table (including images?)
+    
